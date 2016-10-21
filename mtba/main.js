3963fde9669f33554a4a6e833a49b84430e0d6e4
Joel Reske <joelreske@gmail.com>
2016-10-20 20:24:14 -0400

    Completed all major portions of mtba project

---
+++ mtba/main.js
@@ -0,0 +1,189 @@
+var myLat = 0;
+var myLng = 0;
+var request = new XMLHttpRequest();
+var me = new google.maps.LatLng(myLat, myLng);
+var map;
+var my_marker;
+var redline_data_url = "https://rocky-taiga-26352.herokuapp.com/redline.json";
+
+var myOptions = {
+    zoom: 13,
+    center: me,
+    mapTypeId: google.maps.MapTypeId.ROADMAP
+};
+
+
+function init() {
+    map = new google.maps.Map(document.getElementById("map_canvas"), myOptions);
+    showMyLocation();
+}
+
+function showMyLocation() {
+    if (navigator.geolocation) { // the navigator.geolocation object is supported on your browser
+        navigator.geolocation.getCurrentPosition(function(position) {
+            myLat = position.coords.latitude;
+            myLng = position.coords.longitude;
+
+            myPos = new google.maps.LatLng(myLat, myLng);
+            map.panTo(myPos);
+            my_marker = new google.maps.Marker({
+                position: myPos,
+                title: "Here I Am!"
+            });
+            my_marker.setMap(map);
+            showStops();
+        });
+    } else {
+        alert("Geolocation is not supported by your web browser.  What a shame!");
+    }
+}
+
+var closest_stop;
+
+function showStops() {
+    var paths = new Array(4);
+    paths[1] = [];
+    paths[2] = [];
+    paths[3] = [];
+
+    for (i in stops) {
+        var cur_stop = stops[i];
+
+        var cur_marker = new google.maps.Marker({
+            position: {
+                lat: cur_stop["stop_lat"],
+                lng: cur_stop["stop_lon"]
+            },
+            icon: "t_logo.png",
+            map: map
+        });
+
+        var cur_pos = {
+            lat: cur_stop["stop_lat"],
+            lng: cur_stop["stop_lon"]
+        };
+        paths[cur_stop["line"]].push(cur_pos);
+
+        addStopInfo(cur_marker, cur_stop["stop_name"]);
+
+        // Calculate closest distance
+        cur_stop["dist"] = haversineDistance([myLat, myLng], [cur_stop["stop_lat"], cur_stop["stop_lon"]]);
+        if (closest_stop == null || cur_stop["dist"] < closest_stop["dist"]) {
+            closest_stop = cur_stop;
+        }
+    }
+    updateClosest();
+
+    /* Draw lines connecting paths*/
+    for (i in paths) {
+        if (i > 1) {
+            paths[i].unshift({
+                "lat": 42.320685,
+                "lng": -71.052391
+            });
+        }
+        var line = new google.maps.Polyline({
+            path: paths[i],
+            geodesic: true,
+            strokeColor: '#FF0000',
+            strokeOpacity: 1.0,
+            strokeWeight: 2
+        });
+        line.setMap(map);
+    }
+}
+
+function addStopInfo(stop_marker, name) {
+    var stop_info = new google.maps.InfoWindow({
+        content: name
+    });
+
+    stop_marker.addListener('click', function() {
+
+        function showSchedule(schedule) {
+            content = "<b>" + name + "</b><br>";
+            var cur_time = schedule.TripList.CurrentTime;
+            for (i in schedule.TripList.Trips) {
+                var cur_trip = schedule.TripList.Trips[i];
+
+                for (j in cur_trip.Predictions) {
+                    var cur_predict = cur_trip.Predictions[j];
+                    if (cur_predict.Stop == name) {
+                        var arrival_time = cur_time + cur_predict.Seconds;
+                        var arrival_date = new Date(arrival_time * 1000);
+                        content += "Train to <b>" + cur_trip.Destination + "</b> arriving at <b>" + arrival_date.toLocaleTimeString() + "</b><br>";
+                    }
+                }
+            }
+            stop_info.setContent(content);
+            stop_info.open(stop_marker.get('map'), stop_marker);
+        }
+        request.onreadystatechange = function() {
+            if (request.readyState == 4) {
+                if (request.status == 200) {
+                    showSchedule(JSON.parse(request.responseText));
+                } else {
+                    alert("Shoot! Looks like the MTBA returned a 404 error. Please try again in a moment.");
+                }
+            }
+        }
+        request.open("GET", redline_data_url, true);
+        request.send(null);
+    });
+}
+
+function updateClosest() {
+    var closest_line = new google.maps.Polyline({
+        path: [{
+            lat: myLat,
+            lng: myLng
+        }, {
+            lat: closest_stop["stop_lat"],
+            lng: closest_stop["stop_lon"]
+        }],
+        geodesic: true,
+        strokeColor: '#FF0000',
+        strokeOpacity: 1.0,
+        strokeWeight: 2
+    });
+    closest_line.setMap(map);
+
+    my_marker.addListener('click', function() {
+        var title = "<b>Closest Line</b> is " + closest_stop["stop_name"] + "<br> <b>Distance</b>: " + closest_stop["dist"];
+        var my_info = new google.maps.InfoWindow({
+            content: title
+        });
+
+        my_info.open(my_marker.get('map'), my_marker);
+    });
+}
+
+
+/* Based on code found at http://stackoverflow.com/questions/14560999/using-the-haversine-formula-in-javascript */
+function haversineDistance(coords1, coords2, isMiles) {
+    function toRad(x) {
+        return x * Math.PI / 180;
+    }
+
+    var lon1 = coords1[0];
+    var lat1 = coords1[1];
+
+    var lon2 = coords2[0];
+    var lat2 = coords2[1];
+
+    var R = 6371; // km
+
+    var x1 = lat2 - lat1;
+    var dLat = toRad(x1);
+    var x2 = lon2 - lon1;
+    var dLon = toRad(x2)
+    var a = Math.sin(dLat / 2) * Math.sin(dLat / 2) +
+        Math.cos(toRad(lat1)) * Math.cos(toRad(lat2)) *
+        Math.sin(dLon / 2) * Math.sin(dLon / 2);
+    var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+    var d = R * c;
+
+    if (isMiles) d /= 1.60934;
+
+    return d;
+}
---
+++ mtba/stop.js
@@ -0,0 +1,111 @@
+stops = [{
+    "stop_name": "Alewife",
+    "stop_lat": 42.395428,
+    "stop_lon": -71.142483,
+    "line": 1
+}, {
+    "stop_name": "Davis",
+    "stop_lat": 42.39674,
+    "stop_lon": -71.121815,
+    "line": 1
+}, {
+    "stop_name": "Porter Square",
+    "stop_lat": 42.3884,
+    "stop_lon": -71.11914899999999,
+    "line": 1
+}, {
+    "stop_name": "Harvard Square",
+    "stop_lat": 42.373362,
+    "stop_lon": -71.118956,
+    "line": 1
+}, {
+    "stop_name": "Central Square",
+    "stop_lat": 42.365486,
+    "stop_lon": -71.103802,
+    "line": 1
+}, {
+    "stop_name": "Kendall/MIT",
+    "stop_lat": 42.36249079,
+    "stop_lon": -71.08617653,
+    "line": 1
+}, {
+    "stop_name": "Charles/MGH",
+    "stop_lat": 42.361166,
+    "stop_lon": -71.070628,
+    "line": 1
+}, {
+    "stop_name": "Park Street",
+    "stop_lat": 42.35639457,
+    "stop_lon": -71.0624242,
+    "line": 1
+}, {
+    "stop_name": "Downtown Crossing",
+    "stop_lat": 42.355518,
+    "stop_lon": -71.060225,
+    "line": 1
+}, {
+    "stop_name": "South Station",
+    "stop_lat": 42.352271,
+    "stop_lon": -71.05524200000001,
+    "line": 1
+}, {
+    "stop_name": "Broadway",
+    "stop_lat": 42.342622,
+    "stop_lon": -71.056967,
+    "line": 1
+}, {
+    "stop_name": "Andrew",
+    "stop_lat": 42.330154,
+    "stop_lon": -71.057655,
+    "line": 1
+}, {
+    "stop_name": "JFK/UMass",
+    "stop_lat": 42.320685,
+    "stop_lon": -71.052391,
+    "line": 1
+}, {
+    "stop_name": "North Quincy",
+    "stop_lat": 42.275275,
+    "stop_lon": -71.029583,
+    "line": 2
+}, {
+    "stop_name": "Wollaston",
+    "stop_lat": 42.2665139,
+    "stop_lon": -71.0203369,
+    "line": 2
+}, {
+    "stop_name": "Quincy Center",
+    "stop_lat": 42.251809,
+    "stop_lon": -71.005409,
+    "line": 2
+}, {
+    "stop_name": "Quincy Adams",
+    "stop_lat": 42.233391,
+    "stop_lon": -71.007153,
+    "line": 2
+}, {
+    "stop_name": "Braintree",
+    "stop_lat": 42.2078543,
+    "stop_lon": -71.0011385,
+    "line": 2
+}, {
+    "stop_name": "Savin Hill",
+    "stop_lat": 42.31129,
+    "stop_lon": -71.053331,
+    "line": 3
+}, {
+    "stop_name": "Fields Corner",
+    "stop_lat": 42.300093,
+    "stop_lon": -71.061667,
+    "line": 3
+}, {
+    "stop_name": "Shawmut",
+    "stop_lat": 42.29312583,
+    "stop_lon": -71.06573796000001,
+    "line": 3
+}, {
+    "stop_name": "Ashmont",
+    "stop_lat": 42.284652,
+    "stop_lon": -71.06448899999999,
+    "line": 3
+}, ]
